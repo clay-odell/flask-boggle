4c64c56d817636cce409ec347614664475293e2d
Clay O'Dell <clay.odell.music@gmail.com>
2024-05-20 14:26:35 -0500

    boggle board on '/' route and session board added

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, request, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysupersecretkey1302"
+
 boggle_game = Boggle()
+
+
+@app.route('/')
+def home():
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('index.html', board=board)
