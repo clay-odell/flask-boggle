cbaf9f2f6ab3a51fc87111a99580192637186b88
Clay O'Dell <clay.odell.music@gmail.com>
2024-05-20 23:10:54 -0500

    error/success messaging added to boggle.js, updated index.html

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = "mysupersecretkey1302"
+toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
@@ -12,3 +15,10 @@ def home():
     board = boggle_game.make_board()
     session['board'] = board
     return render_template('index.html', board=board)
+
+@app.route('/word-submit')
+def word_submit():
+    board = session["board"]
+    word = request.args["word"]
+    response = boggle_game.check_valid_word(board, word)
+    return jsonify({'result': response})
