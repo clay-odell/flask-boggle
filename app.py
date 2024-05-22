b60c65aaf42102549dcc1c1cb7c716c44574bc1d
Clay O'Dell <clay.odell.music@gmail.com>
2024-05-22 14:32:18 -0500

    test_home added

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, session, jsonify
-from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 app = Flask(__name__)
-app.debug = True
+
 app.config['SECRET_KEY'] = "mysupersecretkey1302"
-toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
--- test.py
+++ test.py
@@ -3,8 +3,28 @@ from app import app
 from flask import session
 from boggle import Boggle
 
+import unittest
 
 class FlaskTests(TestCase):
+    def setUp(self):
+        self.app = app
+        self.client = app.test_client()
+        
+    def test_home(self):
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [['Z', 'T', 'N', 'V', 'F'], ['I', 'C', 'Y', 'K', 'Q'], ['N', 'L', 'X', 'X', 'B'],['Y', 'S', 'T', 'A', 'R'], ['K', 'T', 'J', 'B', 'D']]
+                sess['highscore'] = 0
+                sess['attempts'] = 0
 
-    # TODO -- write tests for every view function / feature!
+            response = client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('board', session)
+            self.assertIn('highscore', session)
+            self.assertIn('attempts', session)
+            self.assertIsInstance(session['board'], list)
+            self.assertIsInstance(session['highscore'], int)
+            self.assertIsInstance(session['attempts'], int)
 
+if __name__ == '__main__':
+    unittest.main()
