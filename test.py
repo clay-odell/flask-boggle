de4a497a2be1e2618bc50cbc32edcfb86c75eae4
Clay O'Dell <clay.odell.music@gmail.com>
2024-05-22 15:12:50 -0500

    post_score tests added

--- test.py
+++ test.py
@@ -10,6 +10,7 @@ class FlaskTests(TestCase):
         self.app = app
         self.client = app.test_client()
         
+        
     def test_home(self):
         with self.client as client:
             with client.session_transaction() as sess:
@@ -25,6 +26,42 @@ class FlaskTests(TestCase):
             self.assertIsInstance(session['board'], list)
             self.assertIsInstance(session['highscore'], int)
             self.assertIsInstance(session['attempts'], int)
+
+    def test_word_submit(self):
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['board'] = [['Z', 'T', 'N', 'V', 'F'], ['I', 'C', 'Y', 'K', 'Q'], ['N', 'L', 'X', 'X', 'B'],['Y', 'S', 'T', 'A', 'R'], ['K', 'T', 'J', 'B', 'D']]
+                
+            response = client.get('/word-submit', query_string={'word': 'test'})
+            self.assertEqual(response.status_code, 200)
+            data = response.get_json()
+            self.assertIn('result', data)
+
+    def test_post_score(self):
+        with self.client as client:
+            with client.session_transaction() as sess:
+                sess['highscore'] = 10
+                sess['attempts'] = 5
+
+        response = client.post('/post-score', json={'score': 15})
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertIn('newRecord', data)
+        self.assertTrue(data['newRecord'])
+
+        with client.session_transaction() as sess:
+            self.assertEqual(sess['highscore'], 15)
+            self.assertEqual(sess['attempts'], 6)
+
+        response = client.post('/post-score', json={'score': 10})
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertIn('newRecord', data)
+        self.assertFalse(data['newRecord'])
+
+        with client.session_transaction() as sess:
+            self.assertEqual(sess['highscore'], 15)
+            self.assertEqual(sess['attempts'], 7)
 
 if __name__ == '__main__':
     unittest.main()
